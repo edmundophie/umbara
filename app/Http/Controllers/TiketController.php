6fd705e65945079f7fe125e4823da6d8c3057901
MarioFilino <mario.filino@gmail.com>
2015-04-17 00:24:44 +0700

    tiket selesai

--- app/Http/Controllers/TiketController.php
+++ app/Http/Controllers/TiketController.php
@@ -8,24 +8,65 @@
 
 namespace App\Http\Controllers;
 
+use App\HargaModel;
 use App\Pelanggan;
+use App\PelangganModel;
+use App\StatusModel;
+use App\Tiket;
 use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Str;
 
 class TiketController extends Controller {
+
     public function insert(Request $request){
+        //session stub
+        Session::put('username', 'cho');
+        $username = Session::get('username');
+
         $namaPelanggan = $request->get('nama');
         $alamatPelanggan = $request->get('alamat');
         $teleponPelanggan = $request->get('telepon');
+        $jenis = $request->get('jenis');
+        $tanggal = $request->get('tanggal_berangkat');
+        $jam = $request->get('jam_berangkat').':00:00';
+        $jumlah = $request->get('jumlah');
+        $dropPoint = $request->get('dropping_point');
 
-        $pelanggan = new Pelanggan();
-        $pelanggan->setNama($namaPelanggan);
-        $pelanggan->setAlamat($alamatPelanggan);
-        $pelanggan->setTelepon($teleponPelanggan);
-        $pelanggan->save();
+        if(PelangganModel::where('telepon', $teleponPelanggan)->first() == null){
+            $pelanggan = new Pelanggan();
+            $pelanggan->setNama($namaPelanggan);
+            $pelanggan->setAlamat($alamatPelanggan);
+            $pelanggan->setTelepon($teleponPelanggan);
+            $pelanggan->save();
+        }
 
-        return redirect('/');
+        $idPelanggan = PelangganModel::where('telepon', $teleponPelanggan)->first()['id'];
+
+        $tiket = new Tiket();
+        $tiket->setCSO($username);
+        $tiket->setIdPelanggan($idPelanggan);
+        $tiket->setKeberangkatan($jam, $tanggal);
+        $tiket->setTujuan('Cinere');
+        $tiket->setJumlah($jumlah);
+        $tiket->setIdStatus(StatusModel::where('status', $jenis)->first()['id']);
+        $tiket->setDropPoint($dropPoint);
+        $tiket->setIdHarga(HargaModel::where('deskripsi', $this->dateChecker($tanggal))->first()['id']);
+        $tiket->save();
+
+        return redirect('transaksi');
     }
+
+    public function dateChecker($date){
+        $date = new \DateTime($date);
+        $day = $date->format("w");
+
+        if($day == 6 || $day == 0){
+            return 'weekend';
+        }else{
+            return 'weekday';
+        }
+    }
+
 } 
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -48,7 +48,7 @@
 						<div class="form-group">
 							<label class="col-sm-3 control-label">Jenis</label>
 							<div class="col-sm-9">
-								<select name="jenis" id="" class="form-control">
+								<select name="jenis" class="form-control">
 									<option value="Pembelian">Pembelian</option>
 									<option value="Pemesanan">Pemesanan</option>
 								</select>
@@ -57,29 +57,29 @@
 						<div class="form-group">
 							<label class="col-sm-3 control-label">Tgl berangkat</label>
 							<div class="col-sm-9">
-								<input type="date" class="form-control">
+								<input type="date" name="tanggal_berangkat" class="form-control">
 							</div>
 						</div>
 						<div class="form-group">
 							<label class="col-sm-3 control-label">Jam berangkat</label>
 							<div class="col-sm-9">	
-								<select name="" id="" class="form-control">
-									<option value="">07.00</option>
-									<option value="">09.00</option>
-									<option value="">11.00</option>
-									<option value="">13.00</option>
-									<option value="">15.00</option>
-									<option value="">17.00</option>
-									<option value="">19.00</option>
-									<option value="">21.00</option>
-									<option value="">23.00</option>
+								<select name="jam_berangkat" class="form-control">
+									<option value="7">07.00</option>
+									<option value="9">09.00</option>
+									<option value="11">11.00</option>
+									<option value="13">13.00</option>
+									<option value="15">15.00</option>
+									<option value="17">17.00</option>
+									<option value="19">19.00</option>
+									<option value="21">21.00</option>
+									<option value="23">23.00</option>
 								</select>
 							</div>
 						</div>
 						<div class="form-group">
 							<label class="col-sm-3 control-label">Qty</label>
 							<div class="col-sm-9">
-								<select name="" id="" class="form-control">
+								<select name="jumlah" class="form-control">
 									<option value="1">1 kursi</option>
 									<option value="2">2 kursi</option>
 									<option value="3">3 kursi</option>
@@ -99,7 +99,7 @@
 						<div class="form-group">
 							<label class="col-sm-3 control-label">Dropping Point</label>
 							<div class="col-sm-9">
-								<input type="text" class="form-control" required>
+								<input type="text" name="dropping_point" class="form-control">
 							</div>
 						</div>
 
